1a9809d1e43dd5d6730190aa855f118346490b0e
patrick681 <kamaupatrick332@gmail.com>
2025-05-05 20:13:57 +0300

    .

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,19 @@
-#!/usr/bin/env python3
-
+# 1. No-argument function
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
+# 2. Function with one argument
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
+# 3. Function with default argument
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
+# 4. Function that returns the sum of two numbers
 def add(num1, num2):
-    pass
+    return num1 + num2
 
+# 5. Function that returns half of the input number
 def halve(number):
-    pass
+    return number / 2
